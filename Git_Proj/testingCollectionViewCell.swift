812db5447fbe658772c9e24e395429c29ec11ceb
zeeshan <zeeshansshaikh@gmail.com>
2022-01-31 21:28:23 +0500

    New File Added

---
+++ Git_Proj/testingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  testingCollectionViewCell.swift
+//  Git_Proj
+//
+//  Created by Zeeshan Anwar on 1/31/22.
+//
+
+import UIKit
+
+class testingCollectionViewCell: UICollectionViewCell {
+    
+}
